7d9398a3691bddb13b719b8b7b58ef7257b65eb8
Lucas-Christian <lucas.christian.programmer@gmail.com>
2024-01-29 18:11:28 -0300

    feat: improved initialization

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,19 +1,22 @@
-import { Client, Events } from 'discord.js'
+import { registerEvents, registerCommands } from '@bot/handlers'
+import { Client, Collection } from 'discord.js'
 import { clientOptions } from '@bot/constants/clientOptions'
 import { singleton } from 'tsyringe'
 import { env } from '@config'
 
 @singleton()
 export class Bot {
-	private client
+	private client: Client
 	constructor() {
 		this.client = new Client(clientOptions)
-		this.client.once(Events.ClientReady, readyClient => {
-			console.log(`> [bot] ${readyClient.user.tag}`)
-		})
+		this.client.commands = new Collection()
 	}
 
-	start() {
-		this.client.login(env['DISCORD_BOT_TOKEN'])
+	async start() {
+		await registerEvents(this.client)
+		const commands = await registerCommands(this.client)
+		await this.client.login(env['DISCORD_BOT_TOKEN'])
+		await this.client.application?.commands.set(commands)
+		console.log(`> [bot] registered ${commands.length} commands with success`)
 	}
 }
