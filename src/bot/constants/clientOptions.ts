bbf52acf2da6574ed70f6b574f40f2c0772a7ed0
Lucas-Christian <lucas.christian.programmer@gmail.com>
2024-01-29 18:09:33 -0300

    feat: client options

--- src/bot/constants/clientOptions.ts
+++ src/bot/constants/clientOptions.ts
@@ -1,4 +1,7 @@
-import { GatewayIntentBits, IntentsBitField, Partials } from 'discord.js'
+import { type Collection, GatewayIntentBits, IntentsBitField, Partials } from 'discord.js'
+import type { DiscordCommand } from '@bot/interfaces/discordCommand'
+
+declare module 'discord.js' { export interface Client { commands: Collection<string, DiscordCommand> } }
 
 export const clientOptions = {
 	intents: [
