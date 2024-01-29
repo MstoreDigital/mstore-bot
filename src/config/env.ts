9e002c303155c23c1bfe11b395248141613f4a3c
Lucas-Christian <lucas.christian.programmer@gmail.com>
2024-01-29 18:56:23 -0300

    feat: new envs

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,10 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
-	DISCORD_BOT_TOKEN: z.string()
+	BACK_END_BASE_URL: z.string().url(),
+	DISCORD_BOT_TOKEN: z.string(),
+	API_KEY: z.string(),
+	PORT: z.string()
 })
 
 export const env = envSchema.parse(process.env)
