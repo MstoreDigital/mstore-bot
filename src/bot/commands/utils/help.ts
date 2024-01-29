38fcfbb375dcbed562bac0f0fc99cbfa4f9ecc00
Lucas-Christian <lucas.christian.programmer@gmail.com>
2024-01-29 18:09:09 -0300

    feat: example ping command

---
+++ src/bot/commands/utils/help.ts
@@ -0,0 +1,11 @@
+import { SlashCommandBuilder, CommandInteraction } from 'discord.js'
+
+export const command = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction: CommandInteraction) {
+		await interaction.reply('Pong!')
+	}
+}
+
