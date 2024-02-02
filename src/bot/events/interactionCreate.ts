a2e90fddbf9963738f37ab6271d94c2d42fe9148
Lucas-Christian <lucas.christian.programmer@gmail.com>
2024-02-02 18:54:47 -0300

    refactor: changing to objects

--- src/bot/events/interactionCreate.ts
+++ src/bot/events/interactionCreate.ts
@@ -1,28 +1,28 @@
 import { DiscordEvent } from '@bot/interfaces/discordEvent'
 import { ButtonInteraction, ChatInputCommandInteraction, Events, Interaction } from 'discord.js'
 
-class InteractionCreateEvent extends DiscordEvent {
-	constructor() {
-		super('on', Events.InteractionCreate, async (interaction: Interaction) => {
-			if(interaction.user.bot) return
-			if(interaction.isChatInputCommand()) {
-				this.chatInputCommandExecute(interaction)
-			} else if(interaction.isButton()) {
-				this.buttonInteractionExecute(interaction)
-			}
-		})
-	}
-	private async chatInputCommandExecute(interaction: ChatInputCommandInteraction) {
-		const command = interaction.client.commands.get(interaction.commandName)
-		if (!command) return
-		try {
-			await command.execute(interaction)
-		} catch(error: any) {
-			await interaction.reply({ content: 'error in command execution', ephemeral: true })
+export const event: DiscordEvent = {
+	type: 'on',
+	name: Events.InteractionCreate,
+	execute: async (interaction: Interaction) => {
+		if(interaction.user.bot) return
+		if(interaction.isChatInputCommand()) {
+			await chatInputCommandExecute(interaction)
+		} else if(interaction.isButton()) {
+			buttonInteractionExecute(interaction)
 		}
-	}
-	private buttonInteractionExecute(interaction: ButtonInteraction) {
-		console.log(interaction)
+	},
+}
+async function chatInputCommandExecute(interaction: ChatInputCommandInteraction) {
+	const command = interaction.client.commands.get(interaction.commandName)
+	if (!command) return
+	try {
+		await command.execute(interaction)
+	} catch(error: any) {
+		await interaction.reply({ content: 'error in command execution', ephemeral: true })
 	}
 }
-export const event = new InteractionCreateEvent()
+function buttonInteractionExecute(interaction: ButtonInteraction) {
+	console.log(interaction)
+}
+
