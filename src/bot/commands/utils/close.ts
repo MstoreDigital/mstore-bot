a0759507008cf3d78456005a71057c80c555240c
Thiago Silva <thiagoms98@outlook.com>
2024-08-26 12:05:11 -0300

    feat: ajusting the appearance of history messages

--- src/bot/commands/utils/close.ts
+++ src/bot/commands/utils/close.ts
@@ -20,11 +20,11 @@ export const command: DiscordCommand = {
 		const translate = translations[selectedLanguage as keyof typeof translations]['utils']['close']
 		
 		if (!channel || channelTypeID !== 12) {
-			await interaction.reply(translate['channelTypeInvalid'])
+			await interaction.reply({ content: translate['channelTypeInvalid'], ephemeral: true })
 			return
 		}
 
-		await interaction.reply('Por favor, informe o motivo do encerramento do ticket:')
+		await interaction.reply({ content: translate['closureReasonRequestMessage'], ephemeral: true })
 
 		const filter = (response: any) => response.author.id === interaction.user.id;
 		const collector = new MessageCollector(channel, { filter, max: 1, time: 60000 })
@@ -32,27 +32,29 @@ export const command: DiscordCommand = {
 		collector.on('collect', async (message) => {
 			const reason = message.content
 			const supportHistoryChannelId = '1272975836110127157'
+			const supportDate = interaction.createdAt.toLocaleString(selectedLanguage, {timeZone: 'UTC'})
 			const supportHistoryChannel = interaction.guild?.channels.cache.get(supportHistoryChannelId) as TextChannel
-			const supportHistoryMessage = `
-				**Ticket ID:** ${channel.id}
-				**Data do atendimento:** ${interaction.createdAt}
-				**Usuário:** ${interaction.user.username} (ID: ${interaction.user.id})
-				**Moderador Responsável:** ${interaction.user.tag} (ID: ${interaction.user.id})
-				**Motivo de Encerramento:** ${reason}
-			`
+			const supportHistoryMessage = translate['historyMessage']
+				.replace('{id}', channel.id)
+				.replace('{createdAt}', supportDate)
+				.replace('{username}', interaction.user.username)
+				.replace('{userId}', interaction.user.id)
+				.replace('{moderatorId}', interaction.user.id)
+				.replace('{userTag}', interaction.user.tag)
+				.replace('{reason}', reason)
 
 			if (supportHistoryChannel) {
 				await supportHistoryChannel.send(supportHistoryMessage)
 			} else {
-				await interaction.followUp('Não foi possível encontrar o canal de histórico.')
+				await interaction.followUp({ content: translate['channelNotFound'], ephemeral: true })
 			}
 
-			await channel.delete('Ticket fechado pelo comando /close')
+			await channel.delete()
 		})
 
 		collector.on('end', collected => {
 			if (collected.size === 0) {
-				interaction.followUp('Você não forneceu um motivo para o encerramento do ticket. O canal não será fechado.')
+				interaction.followUp({ content: translate['reasonNotProvided'], ephemeral: true })
 			}
 		})
   }
